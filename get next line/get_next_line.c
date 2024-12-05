6e194edf11a177b65a5c06eb2f988e61b171f2e3
lamlaika rayane <139387044+raynlamlaika@users.noreply.github.com>
2024-12-05 22:07:18 +0100

    Update get_next_line.c

--- get next line/get_next_line.c
+++ get next line/get_next_line.c
@@ -14,41 +14,25 @@
 #include <fcntl.h>
 #include <libc.h>
 
-
-int check(char *p,int i)
-{
-	
-}
 char *get_next_line(int fd)
 {
-	static char	*p;
-	
-	if (fd < 0 || BFZ <= 0 || BFZ > 2147483647)
-		return (NULL);
-	p = (char *) malloc(sizeof(BFZ) + 1);
-	if(!p)
-		return(NULL);
-	while(TRUE)
+	static char	*string;
+
+	string =  malloc(sizeof(BFZ));
+	while (1)
 	{
-		if(check(p,1))
+		if (ft_strchr(*string,'\n'))
 		{
-			
+			return(finish(string));
 		}
 		if ()
 		{
-
+			
 		}
+
+		
 	}
-	return (NULL);
-}
 
-int main()
-{
-	
-	int i  = open ("example.txt",O_RDONLY);
-	char p[10];
 
-	read(i,p,3);
-	printf("%s",p);
 
 }
