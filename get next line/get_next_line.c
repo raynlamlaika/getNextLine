b452c456e28810ba5198d0083a51a84ea942fa41
Rayane Lamlaika <rlamlaik@e1r8p7.1337.ma>
2024-12-13 18:54:05 +0100

    finaalyyyyyyy

--- get next line/get_next_line.c
+++ get next line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rlamlaik <rlamlaik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 18:47:42 by rlamlaik          #+#    #+#             */
-/*   Updated: 2024/12/13 16:51:17 by rlamlaik         ###   ########.fr       */
+/*   Updated: 2024/12/13 18:53:22 by rlamlaik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,31 +21,64 @@ static void	freed(char **point)
 	}
 }
 
-static void	*finishing(char *buffer, char **line)
+static int	readtobuff(int fd, char **buffer)
 {
-	size_t	i;
-	size_t	j;
+	char		*tohold;
+	char		*temp;
+	ssize_t		byts;
 
-	i = 0;
-	j = 0;
-	while (buffer[i] != '\n' || !buffer[i])
-		i++;
-	j = 0;
-	*line = malloc(i + 2);
-	if (!*line)
-		return (NULL);
-	while (i > j && buffer[j] != '\n')
+	tohold = malloc((size_t)BUFFER_SIZE + 1);
+	if (!tohold)
+		return (0);
+	while (!*buffer || !ft_strchr(*buffer, '\n'))
 	{
-		(*line)[j] = buffer[j];
-		j++;
+		byts = read(fd, tohold, BUFFER_SIZE);
+		if (byts <= 0)
+		{
+			freed(&tohold);
+			return (byts);
+		}
+		tohold[byts] = '\0';
+		temp = *buffer;
+		*buffer = ft_strjoin(*buffer, tohold);
+		freed(&temp);
+		if (!*buffer)
+		{
+			freed(&tohold);
+			return (-1);
+		}
 	}
-	if (buffer[i] == '\n')
-		(*line)[j++] = '\n';
-	(*line)[j] = '\0';
-	return (*line);
+	freed(&tohold);
+	return (1);
 }
 
-static void	rest( char **buffer)
+// static void	*finishing(char *buffer, char **line)
+// {
+// 	size_t	i;
+// 	size_t	j;
+
+// 	i = 0;
+// 	j = 0;
+// 	while (buffer[i] != '\n' || !buffer[i])
+// 		i++;
+// 	j = 0;
+// 	*line = malloc(i + 2);
+// 	if (!*line)
+// 		return (NULL);
+// 	while (i > j && buffer[j] != '\n')
+// 	{
+// 		(*line)[j] = buffer[j];
+// 		j++;
+// 	}
+// 	if (buffer[i] == '\n')
+// 		(*line)[j++] = '\n';
+// 	(*line)[j] = '\0';
+// 	return (*line);
+// }
+
+
+
+static void	update_buffer( char **buffer)
 {
 	char	*temp;
 	char	*newline_pos;
@@ -60,39 +93,47 @@ static void	rest( char **buffer)
 	else
 		freed(buffer);
 }
+static char *extract_line(char *buffer)
+{
+	size_t	i;
+	char	*line;
+
+	if (!buffer || !*buffer)
+		return (NULL);
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	line = malloc(i + 2); // Include space for '\n' and '\0'
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+	{
+		line[i] = buffer[i];
+		i++;
+	}
+	if (buffer[i] == '\n')
+		line[i++] = '\n';
+	line[i] = '\0';
+	return (line);
+}
 
 char	*get_next_line(int fd)
 {
 	static char	*buffer;
 	char		*line;
-	char		*tohold;
-	ssize_t		byts;
+	ssize_t		retu;
 
-	line = NULL;
-	if (BUFFER_SIZE > 2147483647 || (read(fd, 0, 0)) < 0 || fd < 0)
-		return (NULL);
-	tohold = malloc((size_t)BUFFER_SIZE + 1);
-	if (!tohold)
-		return (NULL);
-	while (!buffer || !ft_strchr(tohold, '\n'))
-	{
-		byts = read(fd, tohold, BUFFER_SIZE);
-		if (byts <= 0)
-		{
-			freed(&tohold);
-			if (!buffer || *buffer == '\0')
-				return (freed(&buffer), NULL);
-			break ;
-		}
-		tohold[byts] = '\0';
-		buffer = ft_strjoin(buffer, tohold);
-		if (!buffer)
-			return (freed(&tohold), NULL);
-	}
-	freed(&tohold);
-	if (!finishing(buffer, &line))
+	if (BUFFER_SIZE <= 0 || (read(fd, 0, 0)) < 0 || fd < 0)
 		return (freed(&buffer), NULL);
-	rest(&buffer);
+	retu = readtobuff(fd, &buffer);
+	if (retu <= 0)
+	{
+		if (!buffer || *buffer == '\0')
+			return (freed(&buffer), NULL); // Clean up on EOF or error
+	}
+	line = extract_line(buffer);
+	update_buffer(&buffer);
 	return (line);
 }
 
--- get next line/get_next_line.h
+++ get next line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: rlamlaik <rlamlaik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 22:49:07 by rlamlaik          #+#    #+#             */
-/*   Updated: 2024/12/13 16:52:04 by rlamlaik         ###   ########.fr       */
+/*   Updated: 2024/12/13 17:46:25 by rlamlaik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
-#endif
+# endif
 
 char	*get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
